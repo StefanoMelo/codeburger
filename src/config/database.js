7ee768cd0b589b13fff3d668a81b188410a37f94
devcience <devcience@gmail.com>
2023-05-29 19:56:01 -0300

    atualizaando

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'postgres',
+  database: 'burger2-postgres',
+  define: {
+    timestamps: true,
+    underscore: true,
+    underscoreAll: true,
+
+  },
+}
