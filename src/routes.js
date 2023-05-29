c9e0a3e479eaf9063ab3a2aa9714d0618e60a007
devcience <devcience@gmail.com>
2023-05-29 20:10:41 -0300

    Adicionando Controller

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,11 @@
 import { Router } from "express";
-import { v4 } from 'uuid'
 
-import User from "./app/models/User";
+import UserController from "./app/controllers/UserController";
+
 
 const routes = new Router();
 
-routes.get("/",async (request, response) => {
-  const user = await User.create({
-    id: v4(),
-    name: "Rodolfo",
-    email: "rodolfo@email.com",
-    password_hash: "23asd234",
-    admin: true,
-  })
-  return response.json(user);
-});
+routes.post("/users", UserController.store);
+
+
 export default routes;
