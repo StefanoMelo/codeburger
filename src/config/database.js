67b4124a242704de6a5fcb1785bfbef1213a1069
devcience <devcience@gmail.com>
2023-10-26 10:50:39 -0300

    backend

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: "postgres",
+  host: "localhost",
+  username: "postgres",
+  password: "codeburger",
+  dababase: "codeburger",
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
